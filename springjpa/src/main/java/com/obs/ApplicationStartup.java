a60d9d63f108b2c6f29f09a9739eaf75c2abd04b
Boon Seng <boon-seng.ong@hpe.com>
2017-02-10 11:20:40 +0800

    assert the datasource type when application starts to prevent misconfiguration

--- springjpa/src/main/java/com/obs/ApplicationStartup.java
+++ springjpa/src/main/java/com/obs/ApplicationStartup.java
@@ -1,13 +1,22 @@
 package com.obs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 @Component
 public class ApplicationStartup
         implements ApplicationListener<ApplicationReadyEvent> {
 
+    @Autowired
+    DataSource dataSource;
+
+    @Value("${spring.datasource.type}")
+    String dataSourceType;
     /**
      * This event is executed as late as conceivably possible to indicate that
      * the application is ready to service requests.
@@ -15,5 +24,6 @@ public class ApplicationStartup
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
         System.out.println("Application started");
+        assert (dataSource.getClass().getName().equals(dataSourceType));
     }
 }
